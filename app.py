038e136ae07bea9891fab3a675da5b6d9eeec211
Kamala <kamala.ramaswamyk@gmail.com>
2024-06-18 18:20:53 +0530

    Edit artist venue query

--- app.py
+++ app.py
@@ -253,24 +253,85 @@ def show_artist(artist_id):
 #  Update
 #  ----------------------------------------------------------------
 
-# TODO
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
+  form = ArtistForm()
+  artist = Artist.query.get(artist_id)
+  form.name.data = artist.name
+  form.genres.data = artist.genres
+  form.city.data = artist.city
+  form.state.data = artist.state
+  form.phone.data = artist.phone
+  form.website_link.data = artist.website_link
+  form.facebook_link.data = artist.facebook_link
 
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  
+  artist = Artist.query.get(artist_id)
+  try: 
+    artist.name = request.form['name']
+    artist.city = request.form['city']
+    artist.state = request.form['state']
+    artist.phone = request.form['phone']
+    artist.genres = request.form.getlist('genres')
+    artist.image_link = request.form['image_link']
+    artist.facebook_link = request.form['facebook_link']
+    artist.website_link = request.form['website_link']
+    artist.looking_for_venue = True if 'looking_for_venue' in request.form else False 
+    artist.seeking_description = request.form['seeking_description']
+
+    db.session.add(artist)
+    db.session.commit()
+    flash('Artist ' + request.form['name'] +' was successfully updated!')
+
+  except Exception as ex:
+      flash('An error occurred. Artist ' + request.form['name'] + ' could not be be updated.')
+
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
+  form = VenueForm()
+  venue = Venue.query.get(venue_id)
+  form.name.data = venue.name
+  form.city.data = venue.city
+  form.state.data = venue.state
+  form.phone.data = venue.phone
+  form.address.data = venue.address
+  form.genres.data = venue.genres
+  form.facebook_link.data = venue.facebook_link
+  form.image_link.data = venue.image_link
+  form.website_link.data = venue.website_link
+  form.seeking_talent.data = venue.looking_for_talent
+  form.seeking_description.data = venue.seeking_description
 
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
+  venue = Venue.query.get(venue_id)
+
+  try: 
+    venue.name = request.form['name']
+    venue.city = request.form['city']
+    venue.state = request.form['state']
+    venue.address = request.form['address']
+    venue.phone = request.form['phone']
+    venue.genres = request.form.getlist('genres')
+    venue.image_link = request.form['image_link']
+    venue.facebook_link = request.form['facebook_link']
+    venue.website_link = request.form['website_link']
+    venue.looking_for_talent = True if 'looking_for_talent' in request.form else False 
+    venue.seeking_description = request.form['seeking_description']
+
+    db.session.add(venue)
+    db.session.commit()
+    flash('Venue ' + request.form['name'] +' was successfully updated!')
+
+  except Exception as ex:
+      flash('An error occurred. Venue ' + request.form['name'] + ' could not be be updated.')
   
   return redirect(url_for('show_venue', venue_id=venue_id))
 
@@ -312,7 +373,18 @@ def create_artist_submission():
 
 @app.route('/shows')
 def shows():
-  
+  showDetails = db.session.query(Show).join(Artist).join(Venue).all()
+  data = []
+  for details in showDetails:
+    print('show details', details)
+    data.append({
+      "venue_id": details.venue_id,
+      "venue_name": details.venue.name,
+      "artist_id": details.artist_id,
+      "artist_name": details.artist.name, 
+      "artist_image_link": details.artist.image_link,
+      "start_time": details.start_time.strftime('%Y-%m-%d %H:%M:%S')
+    })
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
@@ -323,6 +395,19 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
+  try: 
+    artist_id = request.form['artist_id']
+    venue_id = request.form['venue_id']
+    start_time = request.form['start_time']
+
+    show = Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed')
+
+  except Exception as ex:
+        flash('An error occurred. Show could not be listed.')
+
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
